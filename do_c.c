09f12a1c1a5a32b3665358b58b4ef463fb15e9c2
chacha <sos_die@naver.com>
2021-03-04 02:45:08 +0900

    fix5

--- do_c.c
+++ do_c.c
@@ -5,7 +5,7 @@ void	do_c(va_list ap, fmtdata *data)
 	if (data->minus)
 		ft_putchar(va_arg(ap, int), data);
 	if (data->width)
-		ft_putwidth(*data, 1);
+		ft_putwidth(data, 1);
 	if (!data->minus)
 		ft_putchar(va_arg(ap, int), data);
 	data->rev += 1;
