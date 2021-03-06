9dc80dc985708f87369a98382179a2d6ae39645d
chacha <sos_die@naver.com>
2021-03-06 23:30:45 +0900

    fix

--- format_check.c
+++ format_check.c
@@ -9,32 +9,20 @@ int		format_check(char *format, va_list ap, fmtdata *data)
 	{
 		if (format[i] == '%')
 			i += ft_putchar('%', data);
-		if (format[i] == '0' && !data->width && !data->minus)
-		{
-			i++;
+		else if (format[i++] == '0' && !data->width && !data->minus)
 			data->zero = 1;
-		}
-		if (format[i] == '-')
+		else if (format[i++] == '-')
 		{
 			data->minus = 1;
 			data->zero = 0;
-			i++;
 		}
-		if (format[i] == '*' && data->precision == -1)
-		{
+		else if (format[i++] == '*' && data->precision == -1)
 			data->width = va_arg(ap, int);
-			i++;
-		}
-		if (format[i] >= '0' && format[i] <= '9' && data->precision == -1)
-		{
+		else if (format[i] >= '0' && format[i] <= '9' && data->precision == -1)
 			while (format[i] >= '0' && format[i] <= '9')
 				data->width = data->width * 10 + format[i++] - '0';
-		}
-		if (format[i] == '.')
-		{
-			i++;
+		else if (format[i++] == '.')
 			i += precision_check(&format[i], ap, data);
-		}
 	}
 	return (i);
 }
